7e7eef1d3c97adca70ad7fba32757aa39856e792
Alan Cham <alanchamis@gmail.com>
2018-12-08 23:59:27 -0800

    Include problem prompts

--- solutions/aoc_1_2.py
+++ solutions/aoc_1_2.py
@@ -1,18 +1,37 @@
 #!/usr/local/bin/python3
 
+# You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+
+# For example, using the same list of changes above, the device would loop as follows:
+
+# Current frequency  0, change of +1; resulting frequency  1.
+# Current frequency  1, change of -2; resulting frequency -1.
+# Current frequency -1, change of +3; resulting frequency  2.
+# Current frequency  2, change of +1; resulting frequency  3.
+# (At this point, the device continues from the start of the list.)
+# Current frequency  3, change of +1; resulting frequency  4.
+# Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+# In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+
+# Here are other examples:
+
+# +1, -1 first reaches 0 twice.
+# +3, +3, +4, -2, -4 first reaches 10 twice.
+# -6, +3, +8, +5, -6 first reaches 5 twice.
+# +7, +7, -2, -7, -4 first reaches 14 twice.
+# What is the first frequency your device reaches twice?
+
 import sys
 
-lines = [line.strip() for line in sys.stdin]
-nums = [int(line) for line in lines]
-
 def first_repeated_total(nums):
-	position, total, seen = 0, 0, {0}
-	while (True):
-		num = nums[position % len(nums)]
-		position += 1
-		total += num
-		if (total in seen):
-			return total
+	turn, total, seen = 0, 0, set()
+	while (total not in seen):
 		seen.add(total)
+		num = nums[turn % len(nums)]
+		total += num
+		turn += 1
+	return total
 
-print(first_repeated_total(nums))
+if __name__ == "__main__":	
+	nums = [int(line.strip()) for line in sys.stdin]
+	print(first_repeated_total(nums))
