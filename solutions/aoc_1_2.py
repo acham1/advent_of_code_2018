9f9fbb7efe1119369c7ead8c88bdd766ae98375a
Alan Cham <alanchamis@gmail.com>
2018-12-08 19:15:10 -0800

    Add aoc_1_2 inputs and solutions

--- solutions/aoc_1_2.py
+++ solutions/aoc_1_2.py
@@ -3,4 +3,16 @@
 import sys
 
 lines = [line.strip() for line in sys.stdin]
-print(lines)
+nums = [int(line) for line in lines]
+
+def first_repeated_total(nums):
+	position, total, seen = 0, 0, {0}
+	while (True):
+		num = nums[position % len(nums)]
+		position += 1
+		total += num
+		if (total in seen):
+			return total
+		seen.add(total)
+
+print(first_repeated_total(nums))
